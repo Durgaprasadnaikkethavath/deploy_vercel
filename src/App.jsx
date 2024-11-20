cc7e6b6926960fa0d64602fa8371796727e4fa4a
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-20 18:22:19 +0530

    develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import ImageCarousel from "./Components/ImageCarousel";
+import { BrowserRouter } from "react-router-dom";
+import ShopContextProvider from "./Context/ShopContext";
 
 const App = () => {
   return (
     <div>
-      <h1>New vercel project</h1>
-      <h2>Api Working</h2>
-      <Navbar />
-      <ImageCarousel />
+      <BrowserRouter>
+        <ShopContextProvider>
+          <h1>New vercel project</h1>
+          <h2>Api Working</h2>
+          <Navbar />
+          <ImageCarousel />
+        </ShopContextProvider>
+      </BrowserRouter>
     </div>
   );
 };
