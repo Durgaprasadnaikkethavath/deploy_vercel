e38d5c994b5f419ea5bd4c592d6f51522fb7a23e
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-20 18:34:37 +0530

    error carousel

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ const App = () => {
     <div>
       <BrowserRouter>
         <ShopContextProvider>
+          <h1>Given Error Carousel Images</h1>
           <h1>New vercel project</h1>
           <h2>Api Working</h2>
           <Navbar />
-          <ImageCarousel />
         </ShopContextProvider>
       </BrowserRouter>
     </div>
